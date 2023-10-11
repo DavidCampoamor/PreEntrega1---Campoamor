b87371baa8801b7fc5a524fad83f362984348232
David93 <campoamordavid@gmail.com>
2023-10-11 02:49:23 -0300

    arrys

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function sumarProductos(precio1, precio2, precio3) {
+/*function sumarProductos(precio1, precio2, precio3) {
     // controlar que los precios no sean negativos
     if (precio1 < 0 || precio2 < 0 || precio3 < 0) {
         return "No se pueden ingresar precios negativos";
@@ -17,4 +17,68 @@ console.log (resultado);
 
 for (let x = 1; x <= 5; x++) {
     console.log(x);
-}
+}*/
+
+const productos = [
+    {
+      nombre: "M Gilbert",
+      precio: 57000
+    },
+    {
+      nombre: "M Wayvee",
+      precio: 79000
+    },
+    {
+        nombre: "Old Skool Bolt",
+        precio: 55000
+    },
+    {
+        nombre: "Skate Old Skool",
+        precio: 64000
+      },
+      {
+        nombre: "U Authentic",
+        precio: 34000
+      },
+      {
+          nombre: "U Old Skool",
+          precio: 53000
+      },
+      {
+        nombre: "U Sk8",
+        precio: 57000
+      },
+      {
+          nombre: "W Sk8 Platform",
+          precio: 59000
+      }
+
+  ];
+
+  function buscarProductoPorNombre(nombre) {
+    return productos.find(producto => producto.nombre.toLowerCase() === nombre.toLowerCase());
+  }
+  const productoBuscado = buscarProductoPorNombre("M Gilbert");
+console.log("Producto buscado:", productoBuscado);
+
+function filtrarProductosPorPrecioMaximo(precioMaximo) {
+    return productos.filter(producto => producto.precio <= precioMaximo);
+  }
+
+  const productosFiltrados = filtrarProductosPorPrecioMaximo(60000);
+console.log("Productos filtrados por precio máximo de 60000:", productosFiltrados);
+
+const nombreProducto = prompt("Ingrese el nombre del producto:");
+let encontrado = false;
+
+for (const producto of productos) {
+    if (producto.nombre.toLowerCase() === nombreProducto.toLowerCase()) {
+      alert(`Producto encontrado:\nNombre: ${producto.nombre}\nPrecio: $${producto.precio}`);
+      encontrado = true;
+      break;
+    }
+  }
+
+  if (!encontrado) {
+    alert("Producto no encontrado. Por favor, verifique el nombre ingresado.");
+  }
